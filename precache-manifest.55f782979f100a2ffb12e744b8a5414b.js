ab34b722e9e8aa50d52fd29757e783c808ed2455
rizkygin <rizkygin@gmail.com>
2019-05-22 20:58:03 +0700

    Updates

---
+++ precache-manifest.55f782979f100a2ffb12e744b8a5414b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f812e97468b96cfffba56ed5e3a199e",
+    "url": "/rizkygin.github.io/index.html"
+  },
+  {
+    "revision": "d7defbbec5e880044e79",
+    "url": "/rizkygin.github.io/static/css/main.342f34ba.chunk.css"
+  },
+  {
+    "revision": "9e8f2a670776209a964d",
+    "url": "/rizkygin.github.io/static/js/2.af93b7c8.chunk.js"
+  },
+  {
+    "revision": "d7defbbec5e880044e79",
+    "url": "/rizkygin.github.io/static/js/main.caf279cf.chunk.js"
+  },
+  {
+    "revision": "2c08a0a7f20948e17d13",
+    "url": "/rizkygin.github.io/static/js/runtime~main.426740a3.js"
+  },
+  {
+    "revision": "14b7627b9e293d8235527880e9354771",
+    "url": "/rizkygin.github.io/static/media/2.14b7627b.jpg"
+  },
+  {
+    "revision": "b341b572bb5cad47b5c9d47405a2f10f",
+    "url": "/rizkygin.github.io/static/media/about-img.b341b572.jpg"
+  },
+  {
+    "revision": "08f968ef623607736158c33a5d14b591",
+    "url": "/rizkygin.github.io/static/media/home-bg-2.08f968ef.jpg"
+  }
+]);
